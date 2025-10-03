e48029d2a03a779fc7cefc72bc12342f30a1f96e
simplescholar1 <mohamedytaha@aucegypt.edu>
2025-10-03 22:02:40 +0300

    Draganddropdone

---
+++ My project/Assets/Draggable.cs
@@ -0,0 +1,62 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Draggable : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Vector3 offset;
+    Rigidbody2D rb;
+    private bool dragging = false;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private Vector2 GetMouseWorldPos()
+    {
+
+        Vector3 mousePoint =  Mouse.current.position.ReadValue();
+
+     
+
+
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePoint.x, mousePoint.y, 0));
+
+        
+        worldPos.z=0;
+        return worldPos;
+    }
+    void Update()
+    {
+        if (Mouse.current.leftButton.isPressed)
+        {
+            Vector3 mouseWorldPos = GetMouseWorldPos();
+            RaycastHit2D hit = Physics2D.Raycast(mouseWorldPos, Vector2.zero);
+            if (hit.collider != null && hit.collider.attachedRigidbody == rb)
+            {
+                if (!dragging)
+                { Debug.Log("Start Dragging");
+                    dragging = true;
+                    offset = transform.position - mouseWorldPos;
+                }
+
+            }
+
+        }
+        if (Mouse.current.leftButton.wasReleasedThisFrame && dragging)
+        {
+            dragging = false;
+        }
+    }
+    void FixedUpdate()
+    {
+        
+        if (dragging)
+        { Debug.Log("Dragging phys");
+            Vector2 mouseWorldPos = GetMouseWorldPos();
+            rb.MovePosition(mouseWorldPos + (Vector2)offset);
+        }
+    }
+
+
+}
